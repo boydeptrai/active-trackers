0bdda4434a54dedb000e1e0e0fad68d78a79239f
Tuan Tran <langthamht047@gmail.com>
2022-11-22 09:29:25 -0800

    navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,14 @@
 <template>
-  <header></header>
+  <header>
+    <nav class="bg-at-light-green text-white">
+      <div class="container py-5 px-4 flex flex-col items-center gap-4 sm:flex-row">
+        <div class="flex items-center gap-x-4">
+          <img src="../assets/images/dumbbell-light.png" class="w-14" alt="">
+          <h1 class="text-lg">Active Tracker</h1>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>

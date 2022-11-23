5cde1d900471a76f27e587720ff37d947a2d2683
Tuan Tran <langthamht047@gmail.com>
2022-11-23 03:17:24 -0800

    storing user in state/store

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -7,24 +7,31 @@
         </div>
         <ul class="flex flex-1 justify-end gap-x-10">
           <router-link class="cursor-pointer " :to="{name: 'Home'}">Home</router-link>
-          <router-link class="cursor-pointer " :to="{name: ''}">Create</router-link>
-          <router-link class="cursor-pointer " :to="{name: 'Login'}">Login</router-link>
-          <li class="cursor-pointer">Logout</li>
+          <router-link v-if="user" class="cursor-pointer " :to="{name: ''}">Create</router-link>
+          <router-link v-if="!user" class="cursor-pointer " :to="{name: 'Login'}">Login</router-link>
+          <li v-if="user" @click="logout" class="cursor-pointer">Logout</li>
         </ul>
     </nav>
   </header>
 </template>
 
 <script>
+import {supabase} from '../supabase/init';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import store from '../store/index'
 export default {
   setup() {
     // Get user from store
-
+    const user = computed(() =>store.state.user)
     // Setup ref to router
-
+    const router = useRoute()
     // Logout function
-
-    return {};
+    const logout = async () =>{
+     await supabase.auth.signOut();
+     router.push({name: "Home"})
+    }
+    return {logout,user};
   },
 };
 </script>

c13a73283e0224d7736feaafd6cc7fc8b6134d2d
Tuan Tran <langthamht047@gmail.com>
2022-11-25 04:08:14 -0800

    router guarding

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Create from '../views/Create.vue'
 import ViewWorkout from '../views/ViewWorkout.vue'
+import {supabase} from '../supabase/init'
 
 const routes = [
   {
@@ -59,7 +60,21 @@ const router = createRouter({
 });
 
 // Change document titles
-
+router.beforeEach((to, from, next) =>{
+  document.title = (`${to.meta.title} || Active Tracker`);
+  next()
+})
 // Route guard for auth routes
-
+router.beforeEach((to, from, next) =>{
+  const user = supabase.auth.user();
+  if (to.matched.some((res) =>res.meta.auth)) {
+    if (user) {
+     next();
+     return;
+    }
+    next({name: "Login"});
+    return;
+  }
+  next()
+})
 export default router;
